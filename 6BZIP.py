f8c8f917c9afd9dcfb312f16fcf3b4126bb16ca6
Adarsh raj <122207457+Rajdocs@users.noreply.github.com>
2023-09-19 23:05:50 +0530

    Update 6BZIP.py

--- 6BZIP.py
+++ 6BZIP.py
@@ -1,22 +1,21 @@
-import os.path
+import os
 import sys
+import pathlib
+import zipfile
 
-fname = input("Enter the filename : ")      
+dirName = input("Enter Directory name that you want to backup : ")
 
-if not os.path.isfile(fname):
-    print("File", fname, "doesn't exists")
+if not os.path.isdir(dirName):
+    print("Directory", dirName, "doesn't exists")
     sys.exit(0)
-
-infile = open(fname, "r")
-
-lineList = infile.readlines()
-
-for i in range(20):
-    print(i+1, ":", lineList[i])
     
-word = input("Enter a word : ")
-cnt = 0
-for line in lineList:
-    cnt += line.count(word)
-
-print("The word", word, "appears", cnt, "times in the file")
+curDirectory = pathlib.Path(dirName)
+    
+with zipfile.ZipFile("myZip.zip", mode="w") as archive:
+    for file_path in curDirectory.rglob("*"):
+        archive.write(file_path, arcname=file_path.relative_to(curDirectory))
+    
+if os.path.isfile("myZip.zip"):
+    print("Archive", "myZip.zip", "created successfully")
+else:
+    print("Error in creating zip archive")
